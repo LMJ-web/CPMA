f15aef252b7dc0d52207a86ff0d7f6db741ce061
LMJ <11382765+lin-tianhai@user.noreply.gitee.com>
2024-08-02 17:52:09 +0800

    优化更新

--- LayuiTest/Global.asax.cs
+++ LayuiTest/Global.asax.cs
@@ -6,15 +6,8 @@ namespace CPMA
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-<<<<<<< HEAD
         protected void Application_Start()
         {
-=======
-        public static string session { get; set; }
-        protected void Application_Start()
-        {
-            session = "asd";
->>>>>>> origin/main
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
